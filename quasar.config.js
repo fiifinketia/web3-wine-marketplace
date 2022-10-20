7c924dcd43b03825bf8695ba923039506eeeedb6
Jose Miguel Gonzales <miguel@wiv.io>
2022-10-21 02:26:35 +0800

    Add dotenv compatibility

--- quasar.config.js
+++ quasar.config.js
@@ -13,6 +13,7 @@
 
 
 const { configure } = require('quasar/wrappers');
+const envparser = require('./envparser');
 
 module.exports = configure(function (ctx) {
   return {
@@ -58,6 +59,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
+      env: envparser(),
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // transpile: false,
