060f75919cc0a2d53d83cf72385ff0eb82700fbf
Jose Miguel Gonzales <miguel@wiv.io>
2022-10-21 03:08:56 +0800

    S[0:23] Fetch NFTs from API

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,6 +6,11 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [],
   },
+  {
+    path: '/marketplace',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{path: '', component: () => import('pages/Marketplace-Main/Marketplace-Main.vue')}],
+  },
 
   // Always leave this as last one,
   // but you can also remove it
