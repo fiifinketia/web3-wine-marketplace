26a46bbba3a2144698e7deabfac012e201e6f64e
Jose Miguel Gonzales <miguel@wiv.io>
2022-10-22 00:11:06 +0800

    S[0:23] Display clickable NFT cards

--- src/pages/Marketplace-Main/components/NFT-Selections.vue
+++ src/pages/Marketplace-Main/components/NFT-Selections.vue
@@ -1,5 +1,24 @@
 <template>
   <q-page-container>
+    
+    <q-btn
+      v-for="token in allNFTs"
+      :key="token.tokenID + ',' + token.network + ',' + token.smartContract"
+      flat
+      :ripple="false"
+      no-caps
+    >
+      <q-card class="q-pa-sm main-marketplace-nft-card">
+        <img :src="token.image" />
+        <q-card-section>
+          <div> {{(token.brand).toUpperCase()}} </div>
+        </q-card-section>
+        <q-card-section>
+          <div> {{(token.brand).toUpperCase()}} </div>
+        </q-card-section>
+      </q-card>
+    </q-btn>
+
   </q-page-container>
 </template>
 
@@ -8,6 +27,7 @@
 import { defineComponent } from 'vue';
 import { ListingWithPricingAndImage } from '../models/Response.models';
 import { RetrieveAllNFTs } from '../services/RetrieveTokens';
+import '../../../css/Marketplace/NFT-Selections.css'
 
 export default defineComponent({
   data() {
