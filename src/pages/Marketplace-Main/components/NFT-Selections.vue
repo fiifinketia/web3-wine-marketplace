e88986e0793f72e66452f168b40b8124759e3a91
Jose Miguel Gonzales <miguel@wiv.io>
2022-10-22 03:20:37 +0800

    S[0:23] NFT Brand name, price, font

--- src/pages/Marketplace-Main/components/NFT-Selections.vue
+++ src/pages/Marketplace-Main/components/NFT-Selections.vue
@@ -8,13 +8,39 @@
       :ripple="false"
       no-caps
     >
-      <q-card class="q-pa-sm main-marketplace-nft-card">
-        <img :src="token.image" />
-        <q-card-section>
-          <div> {{(token.brand).toUpperCase()}} </div>
+      <q-card 
+        class="q-pa-sm main-marketplace-nft-card"
+        flat
+      >
+        <img :src="token.image"/>
+        <q-card-section class="q-px-sm q-pt-xs" style="text-align: left;">
+          <span> 
+            {{token.brand}} 
+          </span>
         </q-card-section>
-        <q-card-section>
-          <div> {{(token.brand).toUpperCase()}} </div>
+        <q-card-section class="column items-start main-marketplace-price-container q-py-sm">
+          <span class="main-marketplace-price-header q-pb-xs">
+            Starting from
+          </span>
+          <div v-if="!!token.listingPrice">
+            <div class="row items-end q-gutter-x-xs">
+              <q-img
+                src="../../../assets/icons/currencies/USDC-Icon.svg"
+                style="height: 20px; width: 20px"
+              />
+              <span class="main-marketplace-price-text-b">
+                0.00
+              </span>
+              <span class="main-marketplace-price-text-l">
+                /$ 00.00
+              </span>
+            </div>
+          </div>
+          <div v-else>
+            <span class="main-marketplace-price-text-b">
+              Not available
+            </span>
+          </div>
         </q-card-section>
       </q-card>
     </q-btn>
@@ -27,7 +53,7 @@
 import { defineComponent } from 'vue';
 import { ListingWithPricingAndImage } from '../models/Response.models';
 import { RetrieveAllNFTs } from '../services/RetrieveTokens';
-import '../../../css/Marketplace/NFT-Selections.css'
+import '../../../css/Marketplace/NFT-Selections.css';
 
 export default defineComponent({
   data() {
