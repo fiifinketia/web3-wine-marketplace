0ce2728b98a9c16f1caac8625606fb5cd6d335e3
sceka <scekicm02@gmail.com>
2022-10-24 10:51:49 +0200

    Add tabs for statistics page

--- src/pages/Metadata/components/StatisticsMenu.vue
+++ src/pages/Metadata/components/StatisticsMenu.vue
@@ -2,17 +2,17 @@
   <div class="flex justify-center">
     <q-tabs style="width: 80%" no-caps align="justify">
       <q-tab name="history">
-        <div class="tab-text-clicked">NFT history</div>
+        <div class="tab-text-not-clicked">NFT history</div>
       </q-tab>
-      <q-tab class="tab-text-clicked" name="about">About</q-tab>
-      <q-tab name="wine-maker" class="tab-text-clicked">Wine-maker</q-tab>
+      <q-tab class="tab-text-not-clicked" name="about">About</q-tab>
+      <q-tab name="wine-maker" class="tab-text-not-clicked">Wine-maker</q-tab>
     </q-tabs>
   </div>
 </template>
 
 <script>
 import { defineComponent, ref } from 'vue-demi';
-import '../../../css/Metadata/Statistics.css';
+import '../../../css/Metadata/StatisticsMenu.css';
 export default defineComponent({
   name: 'StatisticsMenu',
   data() {
@@ -20,22 +20,11 @@ export default defineComponent({
       tab: ref('mails'),
     };
   },
+  methods: {},
 });
 </script>
 
 <style>
-/* .q-tab--active .q-tab__label {
-  color: red;
-} */
-
-.q-tab--active {
-  color: red;
-}
-
-.q-tab {
-  color: #9d9d9d;
-}
-
 .q-tab__indicator {
   color: #3586ff;
 }
