729435d7f5d1c231eef29d1c8cef9b6753411d53
Jose Miguel Gonzales <miguel@wiv.io>
2022-10-28 17:01:52 +0800

    S[0:23] Adjusted NFT cards | Added on-hover effect
    - Border radius where necessary
    - Allowance for 2 lines
    - Removed static width of 250px for the cards

--- src/pages/Marketplace-Main/components/NFT-Selections.vue
+++ src/pages/Marketplace-Main/components/NFT-Selections.vue
@@ -1,52 +1,54 @@
 <template>
-  <q-page-container class="row q-pt-none">
-
-    <q-btn
+  <q-page-container class="row q-pt-none q-px-md q-gutter-y-md">
+    <div 
       v-for="token in allNFTs"
       :key="token.tokenID + ',' + token.network + ',' + token.smartContractAddress"
-      flat
-			dense
-      :ripple="false"
-      no-caps
-			class="col-md-3 col-sm-4 col-xs-6 no-outline"
+      class="col-xl-3 col-md-3 col-sm-4 col-xs-6 main-marketplace-card-container"
     >
-      <q-card
-        class="q-pa-xs main-marketplace-nft-card"
+      <q-btn
         flat
+        dense
+        :ripple="false"
+        no-caps
+        class="btn--no-hover"
       >
-        <img :src="token.image"/>
-        <q-card-section class="q-px-sm q-pt-xs" style="text-align: left;">
-          <span>
-            {{token.brand}}
-          </span>
-        </q-card-section>
-        <q-card-section class="column items-start main-marketplace-price-container q-py-sm">
-          <span class="main-marketplace-price-header q-pb-xs">
-            Starting from
-          </span>
-          <div v-if="!!token.listingPrice">
-            <div class="row items-end q-gutter-x-xs">
-              <q-img
-                src="../../../assets/icons/currencies/USDC-Icon.svg"
-                style="height: 20px; width: 20px"
-              />
-              <span class="main-marketplace-price-text-b">
-                0.00
-              </span>
-              <span class="main-marketplace-price-text-l">
-                /$ 00.00
-              </span>
-            </div>
-          </div>
-          <div v-else>
-            <span class="main-marketplace-price-text-b">
-              Not available
+        <q-card
+          class="q-pa-xs main-marketplace-nft-card"
+          flat
+        >
+          <img class="main-marketplace-card-image" :src="token.image"/>
+          <div class="q-px-sm  q-pb-sm main-marketplace-card-brand column justify-center" style="text-align: left;">
+            <span>
+              {{token.brand}}
             </span>
           </div>
-        </q-card-section>
-      </q-card>
-    </q-btn>
-
+          <q-card-section class="column items-start main-marketplace-price-container q-py-sm q-mx-sm">
+            <span class="main-marketplace-price-header q-pb-xs">
+              Starting from
+            </span>
+            <div v-if="!!token.listingPrice">
+              <div class="row items-end q-gutter-x-xs">
+                <q-img
+                  src="../../../assets/icons/currencies/USDC-Icon.svg"
+                  style="height: 20px; width: 20px"
+                />
+                <span class="main-marketplace-price-text-b">
+                  0.00
+                </span>
+                <span class="main-marketplace-price-text-l">
+                  /$ 00.00
+                </span>
+              </div>
+            </div>
+            <div v-else>
+              <span class="main-marketplace-price-text-b">
+                Not available
+              </span>
+            </div>
+          </q-card-section>
+        </q-card>
+      </q-btn>
+    </div>
   </q-page-container>
 </template>
 
@@ -75,6 +77,8 @@ export default defineComponent({
 })
 </script>
 
-<style>
-
+<style scoped>
+  :deep(.q-btn.btn--no-hover .q-focus-helper) {
+    display: none;
+  }
 </style>
