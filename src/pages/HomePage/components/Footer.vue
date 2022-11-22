8f291aba0c226ae02db5c614921c169a630fa441
Marko Scekic <81862323+sceka@users.noreply.github.com>
2022-11-22 15:04:51 +0100

    Add footer

--- src/pages/HomePage/components/Footer.vue
+++ src/pages/HomePage/components/Footer.vue
@@ -1,129 +1,54 @@
 <template>
-	<!-- <div class="footer-container">
-    <div class="q-pl-sm q-pt-md">
-      <q-img src="../../../assets/wiv-logo.png" width="80px" />
-    </div>
-    <div class="flex row justify-between text-left">
-      <div class="first">
-        <div class="flex column q-pl-sm q-pt-md">
-          <a class="footer-bold-text q-py-sm">NFTs</a>
-          <a class="footer-normal-text q-py-sm">New Releases</a>
-          <a class="footer-normal-text q-py-sm">Recommended</a>
-          <a class="footer-bold-text q-py-sm">Favourites</a>
-          <a class="footer-bold-text q-py-sm">Stats</a>
-          <a class="footer-normal-text q-py-sm">Wine Statistics</a>
-        </div>
-        <div class="flex column q-pt-md">
-          <a class="footer-bold-text q-py-sm">Storefront</a>
-          <a class="footer-normal-text q-py-sm">New comings</a>
-          <a class="footer-normal-text q-py-sm">Recommended</a>
-        </div>
-      </div>
-      <div class="second">
-        <div class="flex column q-pt-md">
-          <a class="footer-bold-text q-py-sm">Profile</a>
-          <a class="footer-normal-text q-py-sm">Digital Wine Cellar</a>
-          <a class="footer-normal-text q-py-sm">Offers</a>
-          <a class="footer-normal-text q-py-sm">Trading History</a>
-          <a class="footer-normal-text q-py-sm">Notifications</a>
-          <a class="footer-bold-text q-py-sm">Settings</a>
-        </div>
-        <div class="flex column q-pt-md">
-          <a class="footer-bold-text q-py-sm">Help</a>
-          <a class="footer-normal-text q-py-sm">Contact us</a>
-          <a class="footer-normal-text q-py-sm">FAQs</a>
-          <a class="footer-normal-text q-py-sm">Privacy Policy</a>
-          <a class="footer-normal-text q-py-sm">Terms & Conditions</a>
-        </div>
-      </div>
-      <div class="third">
-        <div class="flex column q-pt-md">
-          <div class="footer-bold-text q-py-sm">Contact info:</div>
-          <div class="footer-normal-text q-py-sm">email@email.com</div>
-          <div class="footer-normal-text q-py-sm">+ 0 000 000 0000</div>
-        </div>
-        <div class="flex column footer-column q-pt-md">
-          <div class="footer-bold-text q-py-sm">Integrations</div>
-          <div class="flex row">
-            <div class="q-pr-md">
-              <q-img src="../../../assets/discord.png" width="20px" />
-            </div>
-            <div class="q-pr-md">
-              <q-img src="../../../assets/twitter.png" width="20px" />
-            </div>
-            <div class="q-pr-md">
-              <q-img src="../../../assets/instagram.png" width="20px" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div> -->
-	<div class="footer-clean">
-		<footer>
-			<div class="footer-container">
-				<div class="column justify-center">
-					<div class="col-md-2 item">
-						<h3>NFTs</h3>
-						<ul>
-							<li><a href="#">New Releases</a></li>
-							<li><a href="#">Recommended</a></li>
-							<li><a href="#">Favourites</a></li>
-							<li><a href="#">Stats</a></li>
-							<li><a href="#">Wine Statistics</a></li>
-						</ul>
+	<div class="footer-main-container flex column">
+		<div>image</div>
+		<div class="flex justify-center">
+			<div class="flex row footer-container justify-between">
+				<div class="flex row justify-between footer-responsive">
+					<div class="flex column">
+						<div>NFTs</div>
+						<div>New Realeases</div>
+						<div>Recommended</div>
+						<div>Favourites</div>
+						<div>Stats</div>
+						<div>Wine Statistics</div>
 					</div>
-					<div class="col-sm-4 col-md-2 item">
-						<h3>Storefront</h3>
-						<ul>
-							<li><a href="#">New commings</a></li>
-							<li><a href="#">Recommended</a></li>
-						</ul>
+					<div class="flex column">
+						<div>Storefront</div>
+						<div>New commings</div>
+						<div>Recommended</div>
 					</div>
-
-					<div class="col-sm-4 col-md-2 item">
-						<h3>Profile</h3>
-						<ul>
-							<li><a href="#">Digital Wine Cellar</a></li>
-							<li><a href="#">Offers</a></li>
-							<li><a href="#">Trading History</a></li>
-							<li><a href="#">Notifications</a></li>
-							<li><a href="#">Settings</a></li>
-						</ul>
+				</div>
+				<div class="flex row justify-between footer-responsive">
+					<div class="flex column">
+						<div>Profile</div>
+						<div>Digital Wine Cellar</div>
+						<div>Offers</div>
+						<div>Trading History</div>
+						<div>Notifications</div>
+						<div>Settings</div>
 					</div>
-					<div class="col-sm-4 col-md-2 item">
-						<h3>help</h3>
-						<ul>
-							<li><a href="#">Contact us</a></li>
-							<li><a href="#">FAQs</a></li>
-							<li><a href="#">Privacy policy</a></li>
-							<li><a href="#">Terms and Conditions</a></li>
-						</ul>
+					<div class="flex column">
+						<div>Help</div>
+						<div>Contact us</div>
+						<div>FAQs</div>
+						<div>Privacy Policy</div>
+						<div>Terms and Conditions</div>
 					</div>
-					<div class="col-sm-4 col-md-2 item">
-						<h3>Contact info</h3>
-						<ul>
-							<li><a href="#">Email@email.com</a></li>
-							<li><a href="#">+0 000 000 0000</a></li>
-							<li><a href="#">Benefits</a></li>
-						</ul>
+				</div>
+				<div class="flex row justify-between footer-responsive">
+					<div class="flex column">
+						<div>Contact info:</div>
+						<div>email@email.com</div>
+						<div>+ 0 000 000 0000</div>
 					</div>
-					<div class="col-sm-4 col-md-2 item">
-						<h3>Integrations</h3>
-						<ul>
-							<li><a href="#">Job openings aaaaa</a></li>
-						</ul>
+					<div class="flex column">
+						<div>Integrations</div>
+						<div>emaila@email.com</div>
 					</div>
 				</div>
 			</div>
-		</footer>
+		</div>
 	</div>
-	<!-- <footer class="footer">
-    <div class="q-pl-sm q-pt-md">
-      <q-img src="../../../assets/wiv-logo.png" width="80px" />
-    </div>
-    <div class=""></div>
-  </footer> -->
 </template>
 
 <script lang="ts">
