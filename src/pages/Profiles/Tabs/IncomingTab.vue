32978fa9921e36f5fc02890d706c788911b887b0
Jose Miguel Gonzales <miguel@wiv.io>
2022-11-29 15:50:41 +0800

    S[1:8] Empty view for Listings, Outgoing/Incoming

--- src/pages/Profiles/Tabs/IncomingTab.vue
+++ src/pages/Profiles/Tabs/IncomingTab.vue
@@ -1,154 +1,159 @@
 <template>
-<q-page class="column items-center" :class="!loadingRequest ? 'justify-center' : ''">
+<q-page class="column items-center" :class="!loadingRequest || emptyRequest ? 'justify-center' : ''">
   <div v-if="!loadingRequest" class="column items-center">
     <LoadingView
       :loadingText="'Loading your incoming offers'"
     />
   </div>
   <div v-else class="column items-center">
-    <IncomingHeaderLg
-      v-if="$q.screen.width > 1020"
-      :incomingAmount="incomingOffers.length"
-    />
-    <IncomingHeaderSm
-      v-else
-      :incomingAmount="incomingOffers.length"
-    />
-    <div class="profile-main-container column">
-      <div class="row q-pa-lg profile-column-name">
-        <span class="incoming-column-nft">
-          NFT
-        </span>
-        <span 
-          v-if="$q.screen.width > 1020"
-          class="incoming-column-price-floor"
-        >
-          Floor Price
-        </span>
-        <span class="incoming-column-price-offered">
-          Offered
-        </span>
-        <span 
-          v-if="$q.screen.width > 1265"
-          class="incoming-column-from"
-        >
-          From
-        </span>
-        <span 
-          v-if="$q.screen.width > 600"
-          class="incoming-column-expire"
-        >
-          Exp On
-        </span>
-        <span class="incoming-column-action">
-          Action
-        </span>
-      </div>
-      <q-separator style="background-color: #5e97ec45 !important" inset class="q-mx-xl" />
-      <div class="profile-nft-container">
-      <div 
-        v-for="offer in incomingOffers"
-        :key="offer.orderHash"
-        class="q-px-lg q-py-md row items-center"
-      >
-        <div class="row items-center incoming-column-nft">
-          <img v-if="$q.screen.width > 1265" :src="offer.image" class="profile-nft-image q-mr-md"/>
-          <span class="profile-nft-brand"> {{ offer.brand }}</span>
-        </div>
-        <div 
-          v-if="$q.screen.width > 1020"
-          class="row items-center incoming-column-price-floor"
-        >
-          <img src="../../../assets/icons/currencies/USDC-Icon.svg" />
-          <span class="profile-nft-number"> {{ !!offer.lowestOffer ? offer.lowestOffer : '0.00' }}  </span>
-        </div>
-        <div 
-          v-if="$q.screen.width > 600"
-          class="row items-center incoming-column-price-offered"
-        >
-          <img src="../../../assets/icons/currencies/USDC-Icon.svg" />
-          <span class="profile-nft-number"> {{ offer.offer }} </span>
-          <q-tooltip
-            v-if="
-              $q.screen.width <= 1265
-              && $q.screen.width > 600
-            "
-            anchor="top start" 
-            self="center start"
-            class="listing-tooltip-container" 
-            :offset="$q.screen.width > 1020 ? [70, 30] : [70, 40]"
+    <div v-if="!emptyRequest" class="column items-center">
+      <IncomingHeaderLg
+        v-if="$q.screen.width > 1020"
+        :incomingAmount="incomingOffers.length"
+      />
+      <IncomingHeaderSm
+        v-else
+        :incomingAmount="incomingOffers.length"
+      />
+      <div class="profile-main-container column">
+        <div class="row q-pa-lg profile-column-name">
+          <span class="incoming-column-nft">
+            NFT
+          </span>
+          <span 
+            v-if="$q.screen.width > 1020"
+            class="incoming-column-price-floor"
           >
-            <div class="column">
-              <div 
-                v-if="$q.screen.width <= 1020"
-                class="row items-center justify-between"
-              >
-                <span class="incoming-tooltip-label">
-                  Floor Price
-                </span>
-                <div class="row items-center">
-                  <img src="../../../assets/icons/currencies/USDC-Icon.svg" />
-                  <span class="incoming-tooltip-text q-pl-xs"> {{ offer.offer }} </span>
-                </div>
-              </div>
-              <div class="row items-center justify-between">
-                <span class="incoming-tooltip-label">
-                  From
-                </span>
-                <div class="row items-center">
-                  <span class="incoming-tooltip-text q-pl-xs"> {{ ReduceAddress(offer.offerer) }} </span>
-                </div>
-              </div>
-            </div>
-          </q-tooltip>
+            Floor Price
+          </span>
+          <span class="incoming-column-price-offered">
+            Offered
+          </span>
+          <span 
+            v-if="$q.screen.width > 1265"
+            class="incoming-column-from"
+          >
+            From
+          </span>
+          <span 
+            v-if="$q.screen.width > 600"
+            class="incoming-column-expire"
+          >
+            Exp On
+          </span>
+          <span class="incoming-column-action">
+            Action
+          </span>
         </div>
+        <q-separator style="background-color: #5e97ec45 !important" inset class="q-mx-xl" />
+        <div class="profile-nft-container">
         <div 
-          v-if="$q.screen.width <= 600"
-          class="incoming-column-price-offered column"
+          v-for="offer in incomingOffers"
+          :key="offer.orderHash"
+          class="q-px-lg q-py-md row items-center"
         >
-          <div class="row q-pb-xs">
+          <div class="row items-center incoming-column-nft">
+            <img v-if="$q.screen.width > 1265" :src="offer.image" class="profile-nft-image q-mr-md"/>
+            <span class="profile-nft-brand"> {{ offer.brand }}</span>
+          </div>
+          <div 
+            v-if="$q.screen.width > 1020"
+            class="row items-center incoming-column-price-floor"
+          >
+            <img src="../../../assets/icons/currencies/USDC-Icon.svg" />
+            <span class="profile-nft-number"> {{ !!offer.lowestOffer ? offer.lowestOffer : '0.00' }}  </span>
+          </div>
+          <div 
+            v-if="$q.screen.width > 600"
+            class="row items-center incoming-column-price-offered"
+          >
             <img src="../../../assets/icons/currencies/USDC-Icon.svg" />
             <span class="profile-nft-number"> {{ offer.offer }} </span>
+            <q-tooltip
+              v-if="
+                $q.screen.width <= 1265
+                && $q.screen.width > 600
+              "
+              anchor="top start" 
+              self="center start"
+              class="listing-tooltip-container" 
+              :offset="$q.screen.width > 1020 ? [70, 30] : [70, 40]"
+            >
+              <div class="column">
+                <div 
+                  v-if="$q.screen.width <= 1020"
+                  class="row items-center justify-between"
+                >
+                  <span class="incoming-tooltip-label">
+                    Floor Price
+                  </span>
+                  <div class="row items-center">
+                    <img src="../../../assets/icons/currencies/USDC-Icon.svg" />
+                    <span class="incoming-tooltip-text q-pl-xs"> {{ offer.offer }} </span>
+                  </div>
+                </div>
+                <div class="row items-center justify-between">
+                  <span class="incoming-tooltip-label">
+                    From
+                  </span>
+                  <div class="row items-center">
+                    <span class="incoming-tooltip-text q-pl-xs"> {{ ReduceAddress(offer.offerer) }} </span>
+                  </div>
+                </div>
+              </div>
+            </q-tooltip>
           </div>
-          <span class="profile-nft-number-highlight"> {{ offer.endTime }} </span>
-        </div>
-        <div 
-          v-if="$q.screen.width > 1265"
-          class="row items-center incoming-column-from"
-        >
-          <span class="profile-nft-number"> {{ ReduceAddress(offer.offerer) }} </span>
-        </div>
-        <div 
-          v-if="$q.screen.width > 600"
-          class="incoming-column-expire"
-        >
-          <span class="profile-nft-number-highlight"> {{ offer.endTime }} </span>
-        </div>
-        <div style="margin-left: -5px;" class="row items-center incoming-column-action">
-          <q-btn
-            v-if="$q.screen.width > 1020"
-            flat
-            unelevated
-            dense
-            no-caps
-            class="profile-accept-btn"
-            @click="AcceptOffer(offer.orderHash)"
+          <div 
+            v-if="$q.screen.width <= 600"
+            class="incoming-column-price-offered column"
           >
-            Accept
-          </q-btn>
-          <q-btn
-            v-else
-            flat
-            unelevated
-            dense
-            no-caps
-            @click="AcceptOffer(offer.orderHash)"
+            <div class="row q-pb-xs">
+              <img src="../../../assets/icons/currencies/USDC-Icon.svg" />
+              <span class="profile-nft-number"> {{ offer.offer }} </span>
+            </div>
+            <span class="profile-nft-number-highlight"> {{ offer.endTime }} </span>
+          </div>
+          <div 
+            v-if="$q.screen.width > 1265"
+            class="row items-center incoming-column-from"
           >
-            <img src="../../../assets/accept.svg"/>
-          </q-btn>
+            <span class="profile-nft-number"> {{ ReduceAddress(offer.offerer) }} </span>
+          </div>
+          <div 
+            v-if="$q.screen.width > 600"
+            class="incoming-column-expire"
+          >
+            <span class="profile-nft-number-highlight"> {{ offer.endTime }} </span>
+          </div>
+          <div style="margin-left: -5px;" class="row items-center incoming-column-action">
+            <q-btn
+              v-if="$q.screen.width > 1020"
+              flat
+              unelevated
+              dense
+              no-caps
+              class="profile-accept-btn"
+              @click="AcceptOffer(offer.orderHash)"
+            >
+              Accept
+            </q-btn>
+            <q-btn
+              v-else
+              flat
+              unelevated
+              dense
+              no-caps
+              @click="AcceptOffer(offer.orderHash)"
+            >
+              <img src="../../../assets/accept.svg"/>
+            </q-btn>
+          </div>
+        </div>
         </div>
       </div>
-      </div>
+    </div>
+    <div v-else class="column items-center">
+      <EmptyView :emptyText="'You do not have incoming offers yet.'" />
     </div>
   </div>
 </q-page>
@@ -162,12 +167,14 @@ import { ordersStore } from 'src/stores/orders-store';
 import IncomingHeaderLg from '../Headers/IncomingHeaderLg.vue';
 import IncomingHeaderSm from '../Headers/IncomingHeaderSm.vue';
 import OrderLoading from '../OrderLoading.vue';
+import Empty from '../EmptyOrders.vue';
 
 export default defineComponent({
   components: {
     IncomingHeaderLg: IncomingHeaderLg,
     IncomingHeaderSm: IncomingHeaderSm,
-    LoadingView: OrderLoading
+    LoadingView: OrderLoading,
+    EmptyView: Empty
   },
 
   data() {
@@ -176,7 +183,8 @@ export default defineComponent({
       store,
       incomingOffers: store.incomingOffers,
       incomingFilter: '',
-      loadingRequest: false
+      loadingRequest: false,
+      emptyRequest: false
     }
   },
 
@@ -186,6 +194,9 @@ export default defineComponent({
       const address = '0xA3873a019aC68824907A3aD99D3e3542376573D0';
       await this.store.setOffers(address);
       this.incomingOffers = this.store.getIncomingOffers;
+    }
+    if (this.incomingOffers.length == 0) {
+      this.emptyRequest = true 
     }
     this.loadingRequest = true;
   },
--- src/pages/Profiles/Tabs/OutgoingTab.vue
+++ src/pages/Profiles/Tabs/OutgoingTab.vue
@@ -1,121 +1,126 @@
 <template>
-<q-page class="column items-center" :class="!loadingRequest ? 'justify-center' : ''">
+<q-page class="column items-center" :class="!loadingRequest || emptyRequest ? 'justify-center' : ''">
   <div v-if="!loadingRequest" class="column items-center">
     <LoadingView
       :loadingText="'Loading your outgoing offers'"
     />
   </div>
   <div v-else class="column items-center">
-    <OutgoingHeaderLg
-      v-if="$q.screen.width > 1020"
-      :outgoingAmount="outgoingOffers.length"
-    />
-    <OutgoingHeaderSm
-      v-else
-      :outgoingAmount="outgoingOffers.length"
-    />
-    <div class="profile-main-container column">
-      <div class="row q-pa-lg profile-column-name">
-        <span class="outgoing-column-nft">
-          NFT
-        </span>
-        <span class="outgoing-column-own-offer">
-          Your offer
-        </span>
-        <span 
-          v-if="$q.screen.width > 600"
-          class="outgoing-column-highest-offer"
-        >
-          Highest Offer
-        </span>
-        <span 
-          v-if="$q.screen.width > 1265"
-          class="outgoing-column-expire"
-        >
-          Exp On
-        </span>
-        <span class="outgoing-column-action">
-          Action
-        </span>
-      </div>
-      <q-separator style="background-color: #5e97ec45 !important" inset class="q-mx-xl" />
-      <div class="profile-nft-container">
-      <div 
-        v-for="offer in outgoingOffers"
-        :key="offer.orderHash"
-        class="q-px-lg q-py-md row items-center"
-      >
-        <div class="row items-center outgoing-column-nft">
-          <img v-if="$q.screen.width > 1265" :src="offer.image" class="profile-nft-image q-mr-md"/>
-          <span class="profile-nft-brand"> {{ offer.brand }}</span>
-        </div>
-        <div 
-          v-if="$q.screen.width > 600"
-          class="row items-center outgoing-column-own-offer"
-        >
-          <img src="../../../assets/icons/currencies/USDC-Icon.svg" />
-          <span class="profile-nft-number"> {{ offer.offer }} </span>
-          <q-tooltip 
-            v-if="
-              $q.screen.width <= 1265
-              && $q.screen.width > 600
-            "
-            anchor="top start" 
-            self="center start"
-            class="outgoing-tooltip-container"
-            :offset="[70, 30]"
+    <div v-if="!emptyRequest" class="column items-center">
+      <OutgoingHeaderLg
+        v-if="$q.screen.width > 1020"
+        :outgoingAmount="outgoingOffers.length"
+      />
+      <OutgoingHeaderSm
+        v-else
+        :outgoingAmount="outgoingOffers.length"
+      />
+      <div class="profile-main-container column">
+        <div class="row q-pa-lg profile-column-name">
+          <span class="outgoing-column-nft">
+            NFT
+          </span>
+          <span class="outgoing-column-own-offer">
+            Your offer
+          </span>
+          <span 
+            v-if="$q.screen.width > 600"
+            class="outgoing-column-highest-offer"
           >
-            <div class="column">
-              <div class="row items-center justify-between">
-                <span class="outgoing-tooltip-label">
-                  Expiration On
-                </span>
-                <span class="outgoing-tooltip-expire q-pl-xs"> {{ offer.endTime }} </span>
-              </div>
-            </div>
-          </q-tooltip>
+            Highest Offer
+          </span>
+          <span 
+            v-if="$q.screen.width > 1265"
+            class="outgoing-column-expire"
+          >
+            Exp On
+          </span>
+          <span class="outgoing-column-action">
+            Action
+          </span>
         </div>
+        <q-separator style="background-color: #5e97ec45 !important" inset class="q-mx-xl" />
+        <div class="profile-nft-container">
         <div 
-          v-if="$q.screen.width > 600"
-          class="row items-center outgoing-column-highest-offer"
+          v-for="offer in outgoingOffers"
+          :key="offer.orderHash"
+          class="q-px-lg q-py-md row items-center"
         >
-          <img src="../../../assets/icons/currencies/USDC-Icon.svg" />
-          <span class="profile-nft-number"> {{ !!offer.highestOffer ? offer.highestOffer : '0.00' }} </span>
-        </div>
-        <div
-          v-if="$q.screen.width > 1265"
-          class="row items-center outgoing-column-expire"
-        >
-          <span class="profile-nft-number-highlight"> {{ offer.endTime }} </span>
-        </div>
-        <div 
-          v-if="$q.screen.width <= 600"
-          class="outgoing-column-own-offer column"
-        >
-          <div class="row q-pb-xs">
+          <div class="row items-center outgoing-column-nft">
+            <img v-if="$q.screen.width > 1265" :src="offer.image" class="profile-nft-image q-mr-md"/>
+            <span class="profile-nft-brand"> {{ offer.brand }}</span>
+          </div>
+          <div 
+            v-if="$q.screen.width > 600"
+            class="row items-center outgoing-column-own-offer"
+          >
             <img src="../../../assets/icons/currencies/USDC-Icon.svg" />
             <span class="profile-nft-number"> {{ offer.offer }} </span>
+            <q-tooltip 
+              v-if="
+                $q.screen.width <= 1265
+                && $q.screen.width > 600
+              "
+              anchor="top start" 
+              self="center start"
+              class="outgoing-tooltip-container"
+              :offset="[70, 30]"
+            >
+              <div class="column">
+                <div class="row items-center justify-between">
+                  <span class="outgoing-tooltip-label">
+                    Expiration On
+                  </span>
+                  <span class="outgoing-tooltip-expire q-pl-xs"> {{ offer.endTime }} </span>
+                </div>
+              </div>
+            </q-tooltip>
           </div>
-          <span class="profile-nft-number-highlight"> {{ offer.endTime }} </span>
+          <div 
+            v-if="$q.screen.width > 600"
+            class="row items-center outgoing-column-highest-offer"
+          >
+            <img src="../../../assets/icons/currencies/USDC-Icon.svg" />
+            <span class="profile-nft-number"> {{ !!offer.highestOffer ? offer.highestOffer : '0.00' }} </span>
+          </div>
+          <div
+            v-if="$q.screen.width > 1265"
+            class="row items-center outgoing-column-expire"
+          >
+            <span class="profile-nft-number-highlight"> {{ offer.endTime }} </span>
+          </div>
+          <div 
+            v-if="$q.screen.width <= 600"
+            class="outgoing-column-own-offer column"
+          >
+            <div class="row q-pb-xs">
+              <img src="../../../assets/icons/currencies/USDC-Icon.svg" />
+              <span class="profile-nft-number"> {{ offer.offer }} </span>
+            </div>
+            <span class="profile-nft-number-highlight"> {{ offer.endTime }} </span>
+          </div>
+          <div style="margin-left: -5px;" class="row items-center outgoing-column-action">
+            <q-btn
+              flat
+              unelevated
+              dense
+            >
+              <img src="../../../assets/trash.svg" />
+            </q-btn>
+            <q-btn
+              flat
+              unelevated
+              dense
+            >
+              <img src="../../../assets/edit.svg" />
+            </q-btn>
+          </div>
         </div>
-        <div style="margin-left: -5px;" class="row items-center outgoing-column-action">
-          <q-btn
-            flat
-            unelevated
-            dense
-          >
-            <img src="../../../assets/trash.svg" />
-          </q-btn>
-          <q-btn
-            flat
-            unelevated
-            dense
-          >
-            <img src="../../../assets/edit.svg" />
-          </q-btn>
         </div>
       </div>
-      </div>
+    </div>
+    <div v-else class="column items-center">
+      <EmptyView :emptyText="'You have not made any offers yet.'" />
     </div>
   </div>
 </q-page>
@@ -128,12 +133,14 @@ import 'src/css/Profile/Component/outgoing.css';
 import OutgoingHeaderLg from '../Headers/OutgoingHeaderLg.vue';
 import OutgoingHeaderSm from '../Headers/OutgoingHeaderSm.vue';
 import OrderLoading from '../OrderLoading.vue';
+import EmptyOrders from '../EmptyOrders.vue';
 
 export default defineComponent({
   components: {
     OutgoingHeaderLg: OutgoingHeaderLg,
     OutgoingHeaderSm: OutgoingHeaderSm,
-    LoadingView: OrderLoading
+    LoadingView: OrderLoading,
+    EmptyView: EmptyOrders
   },
   data() {
     const store = ordersStore();
@@ -141,7 +148,8 @@ export default defineComponent({
       store,
       outgoingOffers: store.outgoingOffers,
       outgoingFilter: '',
-      loadingRequest: false
+      loadingRequest: false,
+      emptyRequest: false
     }
   },
 
@@ -151,6 +159,9 @@ export default defineComponent({
       const address = '0x37B4044A9238C4DB0A97c551D165aee3E8C9f95A';
       await this.store.setOffers(address);
       this.outgoingOffers = this.store.getOutgoingOffers;
+    }
+    if (this.outgoingOffers.length == 0) {
+      this.emptyRequest = true
     }
     this.loadingRequest = true;
   },
