af01fe286abe57f53ebc35551c5ba68a193f5544
Jose Miguel Gonzales <miguel@wiv.io>
2023-02-27 16:50:03 +0800

    S[5:76] Add favorites header | Rearranged divs

--- src/pages/Favourites/FavoritesHeader.vue
+++ src/pages/Favourites/FavoritesHeader.vue
@@ -1,38 +1,56 @@
 <template>
   <div 
-    class="row items-center favorites-search-container"
-    :class="$q.screen.width > 600 ? 'justify-between q-px-md' : 'justify-center'"
+    class="q-pb-md"
+    :class="$q.screen.width > 600 ? 'q-pt-lg' : 'column items-center q-pt-none'"
+    style="width: 100%"
   >
-    <div class="row items-center" :style="$q.screen.width > 600 ? '' : 'display: none'">
-      <span class="favorites-nfts-title">NFTs</span>
-      <span class="favorites-nfts-count q-pl-sm">
-        {{ nftsLength > 0 ? nftsLength : 0 }}
-      </span>
+    <div 
+      v-if="$q.screen.width <= 600"
+      class="bg-gradient_blue-green q-mb-md q-px-md q-py-sm" 
+      style="width: 100%"
+    >
+      <div class="row items-center" style="height: 46.28px">
+        <span class="favorites-nfts-title">NFTs</span>
+        <span class="favorites-nfts-count q-pl-xs">
+          {{ nftsLength > 0 ? nftsLength : 0 }}
+        </span>
+      </div>
     </div>
     <div 
-      class="row justify-center q-gutter-x-sm" 
-      :style="$q.screen.width > 600 ? '' : 'width:100%; flex-wrap: nowrap'"
+      class="row items-center favorites-search-container"
+      :class="$q.screen.width > 600 ? 'justify-between q-px-md' : 'justify-center'"
     >
-      <q-input
-        v-model="searchText"
-        dense
-        outlined
-        color="blue-6"
-        placeholder="Search"
-        class="favorites-search-bar"
-        :input-style="!!searchText ? 'color: #212131' : ''"
+      <div class="row items-center" :style="$q.screen.width > 600 ? '' : 'display: none'">
+        <span class="favorites-nfts-title">NFTs</span>
+        <span class="favorites-nfts-count q-pl-sm">
+          {{ nftsLength > 0 ? nftsLength : 0 }}
+        </span>
+      </div>
+      <div 
+        class="row justify-center q-gutter-x-sm" 
+        :style="$q.screen.width > 600 ? '' : 'width:100%; flex-wrap: nowrap'"
       >
-        <template #prepend>
-          <q-icon name="app:search" />
-        </template>
-      </q-input>
-      <q-btn 
-        dense
-        flat
-        class="favorites-search-btn"
-      >
-        GO
-      </q-btn>
+        <q-input
+          v-model="searchText"
+          dense
+          outlined
+          color="blue-6"
+          placeholder="Search"
+          class="favorites-search-bar"
+          :input-style="!!searchText ? 'color: #212131' : ''"
+        >
+          <template #prepend>
+            <q-icon name="app:search" />
+          </template>
+        </q-input>
+        <q-btn 
+          dense
+          flat
+          class="favorites-search-btn"
+        >
+          GO
+        </q-btn>
+      </div>
     </div>
   </div>
 </template>
