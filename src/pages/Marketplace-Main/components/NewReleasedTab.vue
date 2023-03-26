9fa0cc678fb280ea559d18d22f55fa5572032165
Jose Miguel Gonzales <miguel@wiv.io>
2023-03-27 02:10:19 +0800

    S[6:5] Exceptional bordeaux and super tuscans

--- src/pages/Marketplace-Main/components/NewReleasedTab.vue
+++ src/pages/Marketplace-Main/components/NewReleasedTab.vue
@@ -15,6 +15,7 @@
       </div>
     </div>
     <NewlyListedSection class="q-pa-md"/>
+
     <!-- Newly Minted Section -->
     <div class="releases-tab-container column justify-center">
       <div class="row items-center justify-center q-gutter-x-lg">
@@ -30,6 +31,38 @@
       </div>
     </div>
     <NewlyMintedSection class="q-pa-md"/>
+
+    <!-- Super Tuscans Section -->
+    <div class="releases-tab-container column justify-center">
+      <div class="row items-center justify-center q-gutter-x-lg">
+        <q-img
+          src="../../../assets/small-wine.svg"
+          class="releases-tab-icons"
+        />
+        <span> SUPER TUSCANS </span>
+        <q-img
+          src="../../../assets/small-bottle.svg"
+          class="releases-tab-icons"
+        />
+      </div>
+    </div>
+    <SuperTuscans class="q-pa-md"/>
+
+    <!-- Exceptional Bordeaux Section -->
+    <div class="releases-tab-container column justify-center">
+      <div class="row items-center justify-center q-gutter-x-lg">
+        <q-img
+          src="../../../assets/small-wine.svg"
+          class="releases-tab-icons"
+        />
+        <span> EXCEPTIONAL BORDEAUX </span>
+        <q-img
+          src="../../../assets/small-bottle.svg"
+          class="releases-tab-icons"
+        />
+      </div>
+    </div>
+    <ExceptionalBordeaux class="q-pa-md"/>
   </div>
 </template>
 
@@ -37,12 +70,16 @@
 import { defineComponent } from 'vue';
 import NewlyMinted from './Releases/NewlyMinted.vue';
 import NewlyListed from './Releases/NewlyListed.vue';
+import NewTuscans from './Releases/NewTuscans.vue';
 import 'src/css/Releases/Releases-page.css';
+import NewBordeaux from './Releases/NewBordeaux.vue';
 
 export default defineComponent({
   components: {
     NewlyMintedSection: NewlyMinted,
-    NewlyListedSection: NewlyListed
+    NewlyListedSection: NewlyListed,
+    SuperTuscans: NewTuscans,
+    ExceptionalBordeaux: NewBordeaux
   },
   emits: ['totalTokens'],
 });
---
+++ src/pages/Marketplace-Main/components/Releases/NewTuscans.vue
@@ -0,0 +1,59 @@
+<template>
+  <NFTSelections
+    :nft-selections="newNFTs"
+    :is-loading="isLoading"
+    :errored-out="erroredOut"
+    :section-error="erroredText"
+    @refetch-release="GetSuperTuscans()"
+  />
+</template>
+
+<script lang="ts">
+import axios, { AxiosResponse } from 'axios';
+import { defineComponent } from 'vue';
+import { ListingWithPricingAndImage } from '../../models/Response.models';
+import '../../../../css/Releases/Releases-Selections.css';
+import { useUserStore } from 'src/stores/user-store';
+import NFTSelectionsVue from './NFT-Selections.vue';
+
+export default defineComponent({
+  components: {
+    NFTSelections: NFTSelectionsVue
+  },
+  data() {
+    const userStore = useUserStore();
+    return {
+      userStore,
+      newNFTs: new Array<ListingWithPricingAndImage>(),
+      isLoading: true,
+      erroredOut: false,
+      erroredText: 'Super Tuscan wines'
+    };
+  },
+  async mounted() {
+    await this.GetSuperTuscans();
+  },
+
+  methods: {
+    async GetSuperTuscans() {
+      const url = <string>process.env.RETRIEVE_NEWLY_ADDED_REGION_NFTS_URL;
+      try {
+        this.isLoading = true;
+        await axios
+          .get(`${url}?walletAddress=${this.userStore.walletAddress}&region=Tuscany`)
+          .then(
+            (res: AxiosResponse<ListingWithPricingAndImage[]>) =>
+              (this.newNFTs = res.data)
+          );
+        this.erroredOut = false;
+      } catch {
+        this.erroredOut = true;
+      } finally {
+        this.isLoading = false;
+      }
+    }
+  },
+});
+</script>
+
+<style></style>
