a00d9ea2b23c4028b06892241a29a4876930ccfe
Kelvin Nketia-Achiampong <achiampongk22@gmail.com>
2023-04-09 15:50:05 +0000

    Update shepherd.ts

--- src/boot/shepherd.ts
+++ src/boot/shepherd.ts
@@ -20,13 +20,6 @@ export default boot(async ({ app }) => {
 		useModalOverlay: true,
 	}) as Tour;
 
-
-	// This would start the tour when the applications is loaded
-	// TODO: Checking of the user has already done/completed the tour
-	// Set timeout to wait for the DOM to load
-	setTimeout(() => {
-		tour.start()
-	}, 3000)
 	// Setting the this.$shepherd variable to the tour object we created
 	// Doing this would make it accessible any where the project
 	// As long as the component is Using the Optiond API
