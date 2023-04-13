83dd113072f07e17e18d7c864d397d1695cd86c7
Kelvin Nketia-Achiampong <achiampongk22@gmail.com>
2023-04-13 04:45:12 +0000

    feat: add favorites page touring

--- src/stores/tour-state.ts
+++ src/stores/tour-state.ts
@@ -7,26 +7,33 @@ export const useTourStore = defineStore(
     const homeCompleted = ref(false);
     const marketplaceCompleted = ref(false);
     const metadataCompleted = ref(false);
+		const favoritesCompleted = ref(false);
 
     const setHomeCompleted = () => {
       homeCompleted.value = true;
     };
 
 		const setMarketplaceCompleted = () => {
-      homeCompleted.value = true;
+      marketplaceCompleted.value = true;
     };
 
 		const setMetadataCompleted = () => {
-      homeCompleted.value = true;
+      metadataCompleted.value = true;
     };
+
+		const setFavoritesCompleted = () => {
+			favoritesCompleted.value = true;
+		};
 
     return {
       homeCompleted,
 			marketplaceCompleted,
 			metadataCompleted,
+			favoritesCompleted,
       setHomeCompleted,
 			setMarketplaceCompleted,
 			setMetadataCompleted,
+			setFavoritesCompleted
     };
   },
   { persist: true }
