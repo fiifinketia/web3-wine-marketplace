9f9488d11a07e53cbb2a69b663118d08a9c021f6
Jose Miguel Gonzales <miguel@wiv.io>
2023-04-29 19:42:25 +0800

    S[6:25] Add gif to ongoing txn popup

--- src/pages/SharedPopups/TxnOngoing.vue
+++ src/pages/SharedPopups/TxnOngoing.vue
@@ -1,7 +1,11 @@
 <template>
   <q-dialog persistent>
-    <q-card class="q-pa-sm dialog-confirm-box column items-center" style="width: 425px;">
+    <q-card class="q-pa-sm q-py-lg dialog-confirm-box column items-center" style="width: 425px;">
       <span class="error-header"> Submitting transaction </span>
+      <img
+        src="../../assets/loading-metadata.gif"
+        style="width: 90px"
+      />
       <span class="error-subheader q-py-md" style="width: 90%">
         Please do not close the browser tab until submission is completed.
       </span>
